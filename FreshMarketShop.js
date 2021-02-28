591df220d2dc1eb7eed98f457e0ce986f0321d08
Farhad <77681628+Aliyev1993@users.noreply.github.com>
2021-03-01 00:08:32 +0300

    01.03.2021

--- FreshMarketShop.js
+++ FreshMarketShop.js
@@ -157,3 +157,55 @@ $(document).ready(function() {
         );
         });
     
+        // --------------------------------------------------------------------
+
+
+
+        $('.checkout').click(function(){
+
+            $('.allSite').css({
+                "opacity": "0.1",
+                "z-index": "-1",
+            
+            })
+            
+            $('.checkout-list').show();
+            });
+
+
+
+            $('.close-checkout').click(function(){
+
+                $('.allSite').css({
+                    "opacity": "1",
+                    "z-index": "0",
+                
+                })
+                
+                $('.checkout-list').hide();
+                
+                
+                });
+                
+                
+                
+                $('.button-got-it').click(function(){
+                
+                $('.allSite').css({
+                    "opacity": "1",
+                    "z-index": "0",
+                
+                })
+                
+                $('.checkout-list').hide();
+                
+                
+                });
+                
+                
+
+
+
+
+
+            // ---------------------------------------------------------------------------
